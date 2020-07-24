cb6fc8471ebe79920c61bfcdafe30e101dc237f4
Syahreza Fauzi <demon.fauzi@gmail.com>
2020-07-24 20:49:59 +0700

    Init folders and add newtonsoft library for networking utils

---
+++ zauzi.utils.csharp/Models/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zauzi.utils.csharp.Models
+{
+    public abstract class Response
+    {
+        public bool? success { get; set; } = false;
+        public object data { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ zauzi.utils.csharp/Networking/ResponseStatus.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using zauzi.utils.csharp.Models;
+
+namespace zauzi.utils.csharp.Networking
+{
+    public class ResponseStatus : Response
+    {
+        public T getData<T>(Options options = null)
+        {
+            T result = default(T);
+
+            try
+            {
+                String json = JsonConvert.SerializeObject(this.data);
+                result = JsonConvert.DeserializeObject<T>(json);
+            }
+            catch(Exception ex)
+            {
+                if (options?.Exception ?? false) throw;
+            }
+            
+
+            return result;
+        }
+
+        #region support
+        public sealed class Options
+        {
+            public bool Exception { get; set; } = false;
+        }
+        #endregion
+    }
+}
